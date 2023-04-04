501903aca43b99ec802df9567d9dcb4f20d84118
luoyu <51255902046@stu.ecnu.edu.cn>
2023-04-04 21:20:14 +0800

    标数据

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -4,10 +4,6 @@ import random
 
 
 
-def read_json(file):
-    print("### Loading data ...")
-    train_data = json.load(open("train_data.json", "r", encoding="utf-8"))
-    return train_data
 
 # 数据集
 class DefaultDataset:
@@ -83,10 +79,12 @@ def read_tsv(file: str, istrain: bool = False):
         random.shuffle(id_data)
     return id_data
 
-def read_json(file: str, istrain: bool = False):
+def read_json_for_token_classification(file: str, istrain: bool = False):
     id_data = []
     ds = json.load(open(file, "r", encoding="utf-8"))
     for d in ds:
+        if d["type"] != "1":
+            continue
         x, y = d["text"], d["label"]
         id_data.append({"text":x, "label":y})
     if istrain:
@@ -175,3 +173,57 @@ class DataCollatorForTokenClassification:
 
 
 
+
+
+
+
+
+
+
+
+
+def read_json_for_sequence_classification(file: str, istrain: bool = False):
+    id_data = []
+    ds = json.load(open(file, "r", encoding="utf-8"))
+    for d in ds:
+        if d["type"] == "0" or d["type"] == "1":
+            x, y = d["text"], d["type"]
+            id_data.append({"text":x, "label":y})
+    if istrain:
+        random.shuffle(id_data)
+    return id_data
+
+
+class DataCollatorForSequenceClassification:
+    def __init__(self, tokenizer, max_length: int = 512, padding = "max_length", truncation: bool = True):
+        self.tokenizer = tokenizer
+        self.padding = padding
+        self.truncation = truncation
+        self.max_length = max_length
+
+
+    def __call__(self, batch):
+        features = self.collator_fn(batch)
+        return features
+
+
+    def preprocess(self, item):  # 获取数据
+        source = item["text"]
+        target = item["label"]
+        return source, target
+
+    def collator_fn(self, batch):  # 处理函数，主要将json的list转化为tensor的inputs和labels
+        results = map(self.preprocess, batch)
+        inputs, targets = zip(*results)
+
+        input_tensor = self.tokenizer(inputs,
+                                      truncation=self.truncation,  # 截断
+                                      padding=self.padding,  # 填充
+                                      max_length=self.max_length,
+                                      return_tensors="pt",  # pytorch模型 或 "tf" tensorflow模型
+                                      )
+        labels = [int(t) for t in targets]
+        
+        input_tensor["labels"] = torch.tensor(labels)
+
+        return input_tensor
