89ec019a17f162915af4b33e39cf06eb9d21bd79
luoyu <51255902046@stu.ecnu.edu.cn>
2023-05-23 21:26:57 +0800

    新增pdf格式规则以及对应json格式未标注数据

---
+++ data/深交所业务规则/json_for_sequence_classification/transfer_old_to_new.py
@@ -0,0 +1,10 @@
+import json
+
+j1 = json.load(open("深圳证券交易所债券交易规则.json", "r", encoding="utf-8"))
+j2 = json.load(open("finish_深圳证券交易所债券交易规则.json", "r", encoding="utf-8"))
+
+for j, data in enumerate(j2):
+    if "id" in data:
+        data["id"] = j1[j]["id"]
+
+json.dump(j2, open("t", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
