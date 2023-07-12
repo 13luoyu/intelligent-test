303c7e56d164cab5ce8e5f744544de5396bd47ac
Zhiyi Xue <51255902046@stu.ecnu.edu.cn>
2023-07-12 11:45:36 +0800

    update knowledge process

--- ours/process_knowledge.py
+++ ours/process_knowledge.py
@@ -7,9 +7,13 @@ def process_knowledge(input_file: str, output_file: str, todo_file: str):
     knowledge = {}
     rules = json.load(open(input_file, "r", encoding="utf-8"))
     divided_words = ["、", "以及", "和", "或者", "或"]
+    knowledge_count, cannot_process = 0, 0
     for rule in rules:
         if rule["type"] == "2":
+            knowledge_count += 1
             text = rule["text"]
+            text = text.replace("（", "(")
+            text = text.replace("）", ")")
             unuse_words = ["可以", "应当"]
             for word in unuse_words:
                 text = text.replace(word, "")
@@ -18,7 +22,21 @@ def process_knowledge(input_file: str, output_file: str, todo_file: str):
                 if "下列" in text:
                     label = text.split("包括")[0]
                     content = text.split("包括")[1]
-                    contents = [c.split(")")[1] for c in content.split("；")]
+                    if ")" in content:
+                        contents = [c.split(")")[1] for c in content.split("；")]
+                    else:
+                        contents = content[:content.find("等")]
+                        contents = [content]
+                        new_c = []
+                        for word in divided_words:
+                            for c in contents:
+                                cs = c.split(word)
+                                for ci in cs:
+                                    if ci not in new_c:
+                                        new_c.append(ci)
+                                        if c in new_c and ci != c and (ci in c or c in ci):
+                                            new_c.remove(c)
+                            contents = new_c
                     knowledge[label] = contents
                 else:
                     label = text.split("包括")[0]
@@ -35,12 +53,16 @@ def process_knowledge(input_file: str, output_file: str, todo_file: str):
             elif ("是" in text or "指" in text or "为" in text) and text[0] != "(":
                 if "是" in text:
                     texts = text.split("；")
+                    label = ""
                     for text in texts:
-                        label = text.split("是")[0]
-                        content = text.split("是")[1]
-                        if "指" in content:
-                            content = content[content.find("指") + 1:]
-                        knowledge[label] = content
+                        if "是" not in text and label != "":
+                            knowledge[label] = knowledge[label] + "；" + text
+                        elif "是" in text:
+                            label = text.split("是")[0]
+                            content = text.split("是")[1]
+                            if "指" in content:
+                                content = content[content.find("指") + 1:]
+                            knowledge[label] = content
                 elif "指" in text:
                     texts = text.split("；")
                     for text in texts:
@@ -51,16 +73,25 @@ def process_knowledge(input_file: str, output_file: str, todo_file: str):
                         knowledge[label] = content
                 else:  # 为
                     texts = text.split("；")
+                    label = ""
                     for text in texts:
-                        label = text.split("为")[0]
-                        content = text.split("为")[1]
-                        if "：" in content:
-                            content = content[content.find("：") + 1:]
-                        knowledge[label] = content
+                        if "指" not in text and label != "":
+                            knowledge[label] = knowledge[label] + "；" + text
+                        elif "指" in text:
+                            label = text.split("为")[0]
+                            content = text.split("为")[1]
+                            if "：" in content:
+                                content = content[content.find("：") + 1:]
+                            knowledge[label] = content
             elif text[0] == "(":
-                label = text.split("：")[0]
-                label = label[label.find(")") + 1:]
-                content = text.split("：")[1]
+                if "：" in text:
+                    label = text.split("：")[0]
+                    label = label[label.find(")") + 1:]
+                    content = text.split("：")[1]
+                else:
+                    label = text.split("，")[0]
+                    label = label[label.find(")") + 1:]
+                    content = "，".join(text.split("：")[1:])
                 knowledge[label] = content
             elif "采用" in text:
                 if "：" in text:
@@ -97,16 +128,24 @@ def process_knowledge(input_file: str, output_file: str, todo_file: str):
                     knowledge[label] = contents
             else:
                 todo_fp.write(text + "\n")
+                cannot_process += 1
     # pprint(knowledge)
     if os.path.exists(output_file):
         knowledge_pre = json.load(open(output_file, "r", encoding="utf-8"))
         knowledge.update(knowledge_pre)
     json.dump(knowledge, open(output_file, "w", encoding="utf-8"), ensure_ascii=False, indent=4)
-
-
+    return knowledge_count, cannot_process
 
 
 if __name__ == "__main__":
     # 第一个参数为模型的输出文件，第二个为领域知识保存文件，第三个为算法无法自动处理，需要手动处理的领域知识原文保存文件。
     # 算法处理后，需要人工核对
-    process_knowledge("rules_cache/sco.json", "rules_cache/knowledge.json", "rules_cache/todo_knowledge.json")
+    # process_knowledge("rules_cache/sco.json", "rules_cache/knowledge.json", "rules_cache/todo_knowledge.json")
+
+    a, b = 0, 0
+    for file in os.listdir("../data/业务规则/json_for_sequence_classification/"):
+        if "finish" in file:
+            knowledge_count, cannot_process = process_knowledge("../data/业务规则/json_for_sequence_classification/" + file, "rules_cache/knowledge.json", "rules_cache/todo_knowledge.json")
+            a += knowledge_count
+            b += cannot_process
+    print(f"所有领域知识数目：{a}, 能够处理的数目：{a-b}, 处理率为{round(float(a-b)/float(a)*100.0, 1)}%")
