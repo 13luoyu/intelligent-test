bbe5a9dcdc1d9eeb5f3a9b97b7f9666660da886b
username <imoooo4@qq.com>
2023-10-23 09:31:12 +0800

    update

---
+++ experiment/tc.py
@@ -0,0 +1,143 @@
+import json
+from ours.process_tci_to_tco import token_classification
+import os
+import nltk
+
+def edit_distance(s1, s2):
+    """
+    衡量两个字符串的相似度，计算两个字符串的编辑距离。编辑距离指的是将一个字符串转换成另一个字符串所需的最少操作次数，包括插入、删除、替换操作。
+    """
+    return nltk.edit_distance(s1, s2)
+    # len1, len2 = len(s1), len(s2)
+    # dp = [[0] * (len2+1) for _ in range(len1+1)]
+    # for i in range(len1+1):
+    #     dp[i][0] = i
+    # for j in range(len2+1):
+    #     dp[0][j] = j
+    # for i in range(1, len1+1):
+    #     for j in range(1, len2+1):
+    #         if s1[i-1] == s2[j-1]:
+    #             dp[i][j] = dp[i-1][j-1]
+    #         else:
+    #             dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+    # return dp[len1][len2]
+
+def str_same(s1, s2, threshold):
+    # s1是预测值，s2是真实值
+    distance = edit_distance(s1, s2)
+    score = 1 - (distance / max(len(s1), len(s2)))
+    if score >= threshold:
+        return True
+    else:
+        return False
+
+# 将信息抽取任务的预测和真实值放在一个文件中
+def integrate_tc(real_file, hat_file, out_file):
+    rule1 = json.load(open(real_file, "r", encoding="utf-8"))
+    rule2 = json.load(open(hat_file, "r", encoding="utf-8"))
+
+    f = open(out_file, "w", encoding="utf-8")
+
+    f.write("比较结果：\n\n")
+    for i, rule in enumerate(rule1):
+        if "id" in rule:
+            f.write(f"id: {rule['id']}\n")
+        f.write(f"text: {rule['text']}\n")
+        f.write(f"ir hat: {rule2[i]['label']}\n")
+        f.write(f"ir real: {rule['label']}\n\n")
+
+    f.close()
+
+
+
+# 给定信息抽取模型的输出，计算正确标签的信息数、模型预测的信息数、正确的数目，
+# 并由此计算正确率、召回率以及F1分数。
+def evaluate(in_file, out_file, threshold=0.8):
+    with open(in_file, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    # 该条正确的信息数、预测的信息数、正确的数目
+    real_info, hat_info, correct_info = [], [], []
+    for line in lines:
+        if "text" in line:
+            text = line[6:-1]
+        if "ir hat" in line:
+            line = line[8:-1]
+            hats = line.split(" ")
+        elif "ir real" in line:
+            line = line[9:-1]
+            reals = line.split(" ")
+            # 首先从hats和reals中提取信息，存入数组。数组的每个元素为（信息，信息开始，信息结束）
+
+            def compute_info(labels):
+                infos = []
+                last = "O"
+                a, b = 0, 0  # 句子的开始和结束
+                for i, label in enumerate(labels):
+                    if label == "O":
+                        if last != "O":
+                            b = i
+                            infos.append((last, text[a:b]))
+                        last = label
+                    else:
+                        l = label[2:]
+                        if "B-" in label:
+                            if last != "O":
+                                b = i
+                                infos.append((last, text[a:b]))
+                            a = i
+                            last = l
+                        else:
+                            ...
+                return infos
+            info_hats = compute_info(hats)
+            info_reals = compute_info(reals)
+            
+            real_info.append(len(info_reals))
+            hat_info.append(len(info_hats))
+
+            correct_num = 0
+            for info in info_hats:
+                # 这是精确匹配
+                # if info in info_reals:
+                    # correct_num += 1
+                # 模糊匹配，信息只要大部分被抽取出来就认为正确
+                for info1 in info_reals:
+                    if str_same(info[1], info1[1], threshold):
+                        correct_num += 1
+                        break
+            correct_info.append(correct_num)
+    real_sum, hat_sum, correct_sum = sum(real_info), sum(hat_info), sum(correct_info)
+    precision = correct_sum / hat_sum
+    recall = correct_sum / real_sum
+    F1 = 2 * precision * recall / (precision + recall)
+    with open(out_file, "w", encoding="utf-8") as f:
+        f.write(f"真实信息总数：{real_sum}，预测信息总数：{hat_sum}，其中正确的信息数：{correct_sum}，正确率{round(precision, 4)}，召回率{round(recall, 4)}，F1分数{round(F1, 4)}。")
+    print(f"真实信息总数：{real_sum}，预测信息总数：{hat_sum}，其中完全正确的信息数：{correct_sum}，正确率{round(precision, 4)}，召回率{round(recall, 4)}，F1分数{round(F1, 4)}。")
+
+
+
+
+if __name__ == "__main__":
+
+    for file in sorted(os.listdir('../model/ours/')):
+        if file == 'best_1690658708' or file == 'best_1690329462' or 'checkpoint' in file:
+            continue
+        print(file)
+        # 执行信息抽取
+        rules = json.load(open("../data/rules.json", "r", encoding="utf-8"))
+        json.dump(rules, open("cache/exp1_2_input.json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
+        tco = token_classification(rules, "../data/knowledge.json", "../model/ours/" + file, "../data/tc_data.dict")
+        json.dump(tco, open("cache/exp1_2_output.json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
+        # 整合预测与真实值
+        integrate_tc("../data/rules.json", "cache/exp1_2_output.json", "cache/exp1_2_compare.log")
+        # 评估
+        evaluate("cache/exp1_2_compare.log", "cache/exp1_2_result.log", threshold=0.6)
+    # # 执行信息抽取
+    # rules = json.load(open("../data/rules.json", "r", encoding="utf-8"))
+    # json.dump(rules, open("cache/exp1_2_input.json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
+    # tco = token_classification(rules, "../data/knowledge.json", "../model/ours/best_1690329462", "../data/tc_data.dict")
+    # json.dump(rules, open("cache/exp1_2_output.json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
+    # # 整合预测与真实值
+    # integrate_tc("../data/rules.json", "cache/exp1_2_output.json", "cache/exp1_2_compare.log")
+    # # 评估
+    # evaluate("cache/exp1_2_compare.log", "cache/exp1_2_result.log", threshold=0.6)
--- ours/consistency_checking.py
+++ ours/consistency_checking.py
@@ -1,10 +1,12 @@
 import json
 import pprint
 from transfer.mydsl_to_rules import mydsl_to_rules
+import copy
 
 def consistency_checking(data, source):
     defines, vars, rules = mydsl_to_rules(data)
     conflict_rules = []  # {ids: [id1, id2], reason: ''}
+    general_keys = ['交易市场', '交易方式', '交易品种', '交易方向', '事件', '状态', '操作', '操作人', '操作部分']
     rule_ids = list(rules.keys())
     for i, id1 in enumerate(rule_ids):
         for j, id2 in enumerate(rule_ids):
@@ -14,35 +16,277 @@ def consistency_checking(data, source):
             cons1, cons2, res1, res2 = sorted(rule1['constraints'], key=lambda x:x['key']), sorted(rule2['constraints'], key=lambda x:x['key']), sorted(rule1['results'], key=lambda x:x['key']), sorted(rule2['results'], key=lambda x:x['key'])
             
             then_same = True
-            for i, r1 in enumerate(res1):
-                for r2 in res2:
+            if len(res1) != len(res2):
+                then_same = False
+            else:
+                for ri, r1 in enumerate(res1):
+                    r2 = res2[ri]
                     if r1 != r2:
                         then_same = False
                         break
-                if not then_same:
-                    break
             
             if_same = True
-            for c1 in cons1:
-                for c2 in cons2:
+            if len(cons1) != len(cons2):
+                if_same = False
+            else:
+                for ci, c1 in enumerate(cons1):
+                    c2 = cons2[ci]
                     if c1 != c2:
                         if_same = False
                         break
-                if not if_same:
-                    break
+            
+            if then_same:
+                if if_same:
+                    continue
+                else:
+                    # 判断cons1的key和cons2的key之间的关系
+                    cons1_keys, cons2_keys = [con['key'] for con in cons1], [con['key'] for con in cons2]
+                    cons1_value, cons2_value = [con['value'] for con in cons1], [con['value'] for con in cons2]
+                    # 统计两个规则中的每个key的数量
+                    count1, count2 = {}, {}
+                    for key in cons1_keys:
+                        if key in count1:
+                            count1[key] += 1
+                        else:
+                            count1[key] = 1
+                    for key in cons2_keys:
+                        if key in count2:
+                            count2[key] += 1
+                        else:
+                            count2[key] = 1
+                    cons1_in_cons2, cons2_in_cons1, interact = True, True, False
+                    for key in list(count1.keys()):
+                        if key in count2:
+                            interact = True
+                        elif key in count2 and count1[key] != count2[key] or key not in count2:
+                            cons1_in_cons2 = False
+                    for key in list(count2.keys()):
+                        if key in count1 and count1[key] != count2[key] or key not in count1:
+                            cons2_in_cons1 = False
+                            break
+                    
+                    if cons1_in_cons2 and cons2_in_cons1:  # key相同
+                        general_diff = False
+                        other_diff_idx = []
+                        for vi, v1 in enumerate(cons1_value):
+                            v2 = cons2_value[vi]
+                            if v1 != v2 and cons1_keys[vi] in general_keys:
+                                general_diff = True
+                                break
+                            elif v1 != v2 and cons1_keys[vi] not in general_keys:
+                                other_diff_idx.append(vi)
+                        if general_diff:
+                            # 不冲突
+                            continue
+                        else:
+                            # 冲突
+                            reason = f"规则{id1}和{id2}的结果相同，但存在互相冲突的约束：对规则{id1}，"
+                            for idx in other_diff_idx:
+                                reason += f"{cons1_keys[idx]}为{cons1_value[idx]}，"
+                            reason += f"而对规则{id2}，"
+                            for idx in other_diff_idx:
+                                reason += f"{cons2_keys[idx]}为{cons2_value[idx]}，"
+                            reason = f"{reason[:-1]}。"
+                            conflict_rules.append({"rule_ids":[id1, id2], "reason":reason})
+                    elif cons1_in_cons2 or cons2_in_cons1:
+                        # 判断相交部分的value是否完全相同
+                        interact_same = True
+                        if cons1_in_cons2:
+                            for ki, key1 in enumerate(cons1_keys):
+                                kv_diff = True
+                                for kj, key2 in enumerate(cons2_keys):
+                                    if key1 == key2 and cons1_value[ki] == cons2_value[kj]:
+                                        kv_diff = False
+                                        break
+                                if kv_diff:
+                                    interact_same = False
+                                    break
+                            # 将cons2_keys和cons2_value设置为交集部分
+                            interact_cons2_keys = copy.deepcopy(cons1_keys)
+                            interact_cons2_value = []
+                            for ki, key in enumerate(interact_cons2_keys):
+                                v1 = cons1_keys[ki]
+                                last = ""
+                                for vi, v2 in enumerate(cons2_value):
+                                    if key == cons2_keys[vi] and v1 == v2:
+                                        interact_cons2_value.append(v1)
+                                        last = ""
+                                        break
+                                    elif key == cons2_keys[vi] and v1 != v2:
+                                        last = v2
+                                if last != "":
+                                    interact_cons2_value.append(last)
+                            cons2_keys = interact_cons2_keys
+                            cons2_value = interact_cons2_value
+                        else:
+                            for ki, key2 in enumerate(cons2_keys):
+                                kv_diff = True
+                                for kj, key1 in enumerate(cons1_keys):
+                                    if key1 == key2 and cons1_value[kj] == cons2_value[ki]:
+                                        kv_diff = False
+                                        break
+                                if kv_diff:
+                                    interact_same = False
+                                    break
+                            # 将cons1_keys和cons1_value设置为交集部分
+                            interact_cons1_keys = copy.deepcopy(cons2_keys)
+                            interact_cons1_value = []
+                            for ki, key in enumerate(interact_cons1_keys):
+                                v2 = cons2_keys[ki]
+                                last = ""
+                                for vi, v1 in enumerate(cons1_value):
+                                    if key == cons1_keys[vi] and v1 == v2:
+                                        interact_cons1_value.append(v1)
+                                        last = ""
+                                        break
+                                    elif key == cons1_keys[vi] and v1 != v2:
+                                        last = v1
+                                if last != "":
+                                    interact_cons1_value.append(last)
+                            cons2_keys = interact_cons1_keys
+                            cons2_value = interact_cons1_value
+                        if interact_same:
+                            continue
+                        else:
+                            general_diff = False
+                            other_diff_idx = []
+                            for vi, v1 in enumerate(cons1_value):
+                                v2 = cons2_value[vi]
+                                if v1 != v2 and cons1_keys[vi] in general_keys:
+                                    general_diff = True
+                                    break
+                                elif v1 != v2 and cons1_keys[vi] not in general_keys:
+                                    other_diff_idx.append(vi)
+                            if general_diff:
+                                # 不冲突
+                                continue
+                            else:
+                                # 冲突
+                                reason = f"规则{id1}和{id2}的结果相同，但存在互相冲突的约束：对规则{id1}，"
+                                for idx in other_diff_idx:
+                                    reason += f"{cons1_keys[idx]}为{cons1_value[idx]}，"
+                                reason += f"而对规则{id2}，"
+                                for idx in other_diff_idx:
+                                    reason += f"{cons2_keys[idx]}为{cons2_value[idx]}，"
+                                reason = f"{reason[:-1]}。"
+                                conflict_rules.append({"rule_ids":[id1, id2], "reason":reason})
 
+                    elif interact:
+                        continue
+                    else:
+                        continue
+
+
+            else:
+                if if_same:
+                    # 冲突
+                    conflict_rules.append({"rule_ids":[id1, id2], "reason":f"规则{id1}和{id2}的约束相同，但却产生了不同的结果"})
+                else:
+                    # 判断cons1的key和cons2的key之间的关系
+                    cons1_keys, cons2_keys = [con['key'] for con in cons1], [con['key'] for con in cons2]
+                    cons1_value, cons2_value = [con['value'] for con in cons1], [con['value'] for con in cons2]
+                    # 统计两个规则中的每个key的数量
+                    count1, count2 = {}, {}
+                    for key in cons1_keys:
+                        if key in count1:
+                            count1[key] += 1
+                        else:
+                            count1[key] = 1
+                    for key in cons2_keys:
+                        if key in count2:
+                            count2[key] += 1
+                        else:
+                            count2[key] = 1
+                    cons1_in_cons2, cons2_in_cons1, interact = True, True, False
+                    for key in list(count1.keys()):
+                        if key in count2:
+                            interact = True
+                        elif key in count2 and count1[key] != count2[key] or key not in count2:
+                            cons1_in_cons2 = False
+                    for key in list(count2.keys()):
+                        if key in count1 and count1[key] != count2[key] or key not in count1:
+                            cons2_in_cons1 = False
+                            break
+                    
+                    if cons1_in_cons2 and cons2_in_cons1:
+                        continue
+                    elif cons1_in_cons2 or cons2_in_cons1:
+                        # 判断相交部分的value是否完全相同
+                        interact_same = True
+                        if cons1_in_cons2:
+                            for ki, key1 in enumerate(cons1_keys):
+                                kv_diff = True
+                                for kj, key2 in enumerate(cons2_keys):
+                                    if key1 == key2 and cons1_value[ki] == cons2_value[kj]:
+                                        kv_diff = False
+                                        break
+                                if kv_diff:
+                                    interact_same = False
+                                    break
+                            # 将cons2_keys和cons2_value设置为交集部分
+                            interact_cons2_keys = copy.deepcopy(cons1_keys)
+                            interact_cons2_value = []
+                            for ki, key in enumerate(interact_cons2_keys):
+                                v1 = cons1_keys[ki]
+                                last = ""
+                                for vi, v2 in enumerate(cons2_value):
+                                    if key == cons2_keys[vi] and v1 == v2:
+                                        interact_cons2_value.append(v1)
+                                        last = ""
+                                        break
+                                    elif key == cons2_keys[vi] and v1 != v2:
+                                        last = v2
+                                if last != "":
+                                    interact_cons2_value.append(last)
+                            cons2_keys = interact_cons2_keys
+                            cons2_value = interact_cons2_value
+                        else:
+                            for ki, key2 in enumerate(cons2_keys):
+                                kv_diff = True
+                                for kj, key1 in enumerate(cons1_keys):
+                                    if key1 == key2 and cons1_value[kj] == cons2_value[ki]:
+                                        kv_diff = False
+                                        break
+                                if kv_diff:
+                                    interact_same = False
+                                    break
+                            # 将cons1_keys和cons1_value设置为交集部分
+                            interact_cons1_keys = copy.deepcopy(cons2_keys)
+                            interact_cons1_value = []
+                            for ki, key in enumerate(interact_cons1_keys):
+                                v2 = cons2_keys[ki]
+                                last = ""
+                                for vi, v1 in enumerate(cons1_value):
+                                    if key == cons1_keys[vi] and v1 == v2:
+                                        interact_cons1_value.append(v1)
+                                        last = ""
+                                        break
+                                    elif key == cons1_keys[vi] and v1 != v2:
+                                        last = v1
+                                if last != "":
+                                    interact_cons1_value.append(last)
+                            cons2_keys = interact_cons1_keys
+                            cons2_value = interact_cons1_value
+                        if interact_same:
+                            # 冲突
+                            if len(cons1_keys) > len(cons2_keys):
+                                f"规则{id2}的约束包含规则{id1}，但它们的结果不一致"
+                            else:
+                                f"规则{id1}的约束包含规则{id2}，但它们的结果不一致"
+                            conflict_rules.append({"rule_ids": [id1, id2], "reason":reason})
+                        else:
+                            continue
+                    
+                    elif interact:
+                        continue
+                    else:
+                        continue
+    return conflict_rules
 
 
 if __name__ == "__main__":
     input_data = json.load(open("rules_cache/consistency_checking_input.json", "r", encoding="utf-8"))
-    consistency_checking(input_data['data'], input_data['source'])
+    conflict_rules = consistency_checking(input_data['data'], input_data['source'])
+    pprint.pprint(conflict_rules)
 
-
-
-
-
-# then相同、then不同
-# key相同（value同，value不同）、key包含、key有交集、key没有交集
-# 通用标签、特殊标签
-# 不冲突、不冲突
 
