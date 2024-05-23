e3ae80cd456ecef05a79e6575a291a2888203c99
xue <51255902046@stu.ecnu.edu.cn>
2024-05-23 16:21:44 +0800

    update

--- experiment/generate_rules_and_testcases_for_all.py
+++ experiment/generate_rules_and_testcases_for_all.py
@@ -25,7 +25,7 @@ def generate_rules_testcases_for_all():
             print(f"文件《{filename}》开始执行")
             begin_time = time.time()
             nlp_process("data/" + file, "cache/setting.json", "cache/sci.json", "cache/sco.json", "cache/tci.json", "cache/tco.json", "cache/r1.mydsl", "../data/domain_knowledge/classification_knowledge.json", "../data/domain_knowledge/terms.txt", sc_model_path, tc_model_path, "../data/data_for_LLM_v1/tc_data.dict")
-            alg_process("cache/r1.mydsl", "cache/r1.json", "cache/r2.json", "cache/r2.mydsl", "cache/r3.json", f"rules_and_testcases_for_experiment/{filename}_rules_{args.model}.mydsl", f"rules_and_testcases_for_experiment/{filename}_testcases_{args.model}.json", "../data/domain_knowledge/classification_knowledge.json", "../data/domain_knowledge/knowledge.json", "cache/relation.json", "cache/explicit_relation.json", "cache/implicit_relation.json")
+            alg_process("cache/r1.mydsl", "cache/r1.json", "cache/r2.json", "cache/r2.mydsl", "cache/r3.json", f"rules_and_testcases_for_all/{filename}_rules_{args.model}.mydsl", f"rules_and_testcases_for_all/{filename}_testcases_{args.model}.json", "../data/domain_knowledge/classification_knowledge.json", "../data/domain_knowledge/knowledge.json", "cache/relation.json", "cache/explicit_relation.json", "cache/implicit_relation.json")
             time_consume = time.time() - begin_time
             print(f"《{filename}》总共消耗时间: {time_consume}")
 
